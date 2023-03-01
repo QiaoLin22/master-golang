cd781c343f121bee07c131a3d4fb994ded3344dd
Qiao Lin <qiaolin@andrew.cmu.edu>
2023-03-01 01:13:11 -0800

    finishing L1

---
+++ L1/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
+type HashReader interface {
+	io.Reader
+	hash() string
+}
+
+func main() {
+	payload := []byte("hello")
+	hashAndBroadcast(NewHashReader(payload))
+}
+
+type hashReader struct {
+	*bytes.Reader
+	buf *bytes.Buffer
+}
+
+func NewHashReader(b []byte) *hashReader {
+	return &hashReader{
+		Reader: bytes.NewReader(b),
+		buf:    bytes.NewBuffer(b),
+	}
+}
+
+func (h *hashReader) hash() string {
+	return hex.EncodeToString(h.buf.Bytes())
+}
+
+func hashAndBroadcast(r HashReader) error {
+	hash := r.hash()
+	fmt.Println(hash)
+
+	return broadcast(r)
+}
+
+func broadcast(r io.Reader) error {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("string of the bytes: ", string(b))
+
+	return nil
+}

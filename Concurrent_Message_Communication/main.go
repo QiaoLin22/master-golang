95b433520aebb9a4fddcd6b045ca61edc2807b4f
Qiao Lin <qiaolin@andrew.cmu.edu>
2023-04-09 03:12:15 -0700

    finishing concurrent msg communication

--- Concurrent_Message_Communication/main.go
+++ Concurrent_Message_Communication/main.go
@@ -11,18 +11,26 @@ type Message struct {
 }
 
 type Server struct {
-	msgch chan Message
+	msgch  chan Message
+	quitch chan struct{}
 }
 
 func (s *Server) StartAndListen() {
+free:
 	for {
 		select {
 		// block here until someone is sending a message to the channel
 		case msg := <-s.msgch:
 			fmt.Printf("received message from: %s payload %s\n", msg.From, msg.Payload)
+		case <-s.quitch:
+			fmt.Println("the server is doing a graceful shutdown")
+			// logic for graceful shutdown
+			break free
 		default:
 		}
 	}
+
+	fmt.Println("the server is shut down!")
 }
 
 func sendMessageToServer(msgch chan Message, payload string) {
@@ -34,16 +42,26 @@ func sendMessageToServer(msgch chan Message, payload string) {
 	msgch <- msg
 }
 
+func gracefulQuitServer(quitch chan struct{}) {
+	close(quitch)
+}
+
 func main() {
 	s := &Server{
-		msgch: make(chan Message),
+		msgch:  make(chan Message),
+		quitch: make(chan struct{}),
 	}
 
 	go s.StartAndListen()
 
 	go func() {
-		time.Sleep(time.Second)
+		time.Sleep(2 * time.Second)
 		sendMessageToServer(s.msgch, "Hello qiaolin")
+	}()
+
+	go func() {
+		time.Sleep(4 * time.Second)
+		gracefulQuitServer(s.quitch)
 	}()
 
 	select {}

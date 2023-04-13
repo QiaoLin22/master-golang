4ec60eeafad07f21b8e6a46e8456957557234c26
Qiao Lin <qiaolin@andrew.cmu.edu>
2023-04-13 02:01:20 -0700

    finishing microservice docker

---
+++ Microservice_Docker_gRPC/client/client.go
@@ -0,0 +1,49 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/QiaoLin22/golang-microservie-gPRC/types"
+)
+
+type client struct {
+	endpoint string
+}
+
+func New(endpoint string) *client {
+	return &client{
+		endpoint: endpoint,
+	}
+}
+
+func (c *client) FetchPrice(ctx context.Context, ticker string) (*types.PriceResponse, error) {
+	endpoint := fmt.Sprintf("%s?ticker=%s", c.endpoint, ticker)
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		httpErr := map[string]any{}
+		if err := json.NewDecoder(resp.Body).Decode(&httpErr); err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("service respond with non OK status code: %s", httpErr["error"])
+	}
+
+	priceResp := new(types.PriceResponse)
+	if err := json.NewDecoder(resp.Body).Decode(priceResp); err != nil {
+		return nil, err
+	}
+
+	return priceResp, nil
+}
--- Microservice_Docker_gRPC/main.go
+++ Microservice_Docker_gRPC/main.go
@@ -5,6 +5,17 @@ import (
 )
 
 func main() {
+	// client := client.New("http://localhost:3000")
+
+	// price, err := client.FetchPrice(context.Background(), "ETH")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Printf("%+v\n", price)
+
+	// return
+
 	listenAddr := flag.String("listenaddr", ":3000", "listen address the service is running")
 	flag.Parse()
 	svc := NewLoggingService(NewMetricService(&priceFetcher{}))

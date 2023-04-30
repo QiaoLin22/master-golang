edab23f9c7dacdaaf61088fb92ad2fc627d51f11
Qiao Lin <qiaolin@andrew.cmu.edu>
2023-04-29 23:49:25 -0700

    decorator pattern

---
+++ Decorator_Pattern/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type DB interface {
+	Store(string) error
+}
+
+type Store struct{}
+
+func (s *Store) Store(value string) error {
+	fmt.Println("storing into db", value)
+	return nil
+}
+
+func myExecuteFunc(db DB) ExecuteFn {
+	return func(s string) {
+		fmt.Println("my ex func", s)
+		db.Store(s)
+	}
+}
+
+func main() {
+	s := &Store{}
+	Execute(myExecuteFunc(s))
+}
+
+// This is coming from a third party lib
+type ExecuteFn func(string)
+
+func Execute(fn ExecuteFn) {
+	fn("FOO BAR BAZ")
+}
